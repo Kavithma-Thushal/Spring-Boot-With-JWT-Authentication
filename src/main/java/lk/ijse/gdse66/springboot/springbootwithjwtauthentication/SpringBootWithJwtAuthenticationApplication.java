23c5643fdef763f246880c09e1cc6d87928517fc
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-19 15:44:39 +0530

    Model Mapper Bean Added

--- src/main/java/lk/ijse/gdse66/springboot/springbootwithjwtauthentication/SpringBootWithJwtAuthenticationApplication.java
+++ src/main/java/lk/ijse/gdse66/springboot/springbootwithjwtauthentication/SpringBootWithJwtAuthenticationApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse66.springboot.springbootwithjwtauthentication;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWithJwtAuthenticationApplication {
@@ -10,4 +12,8 @@ public class SpringBootWithJwtAuthenticationApplication {
         SpringApplication.run(SpringBootWithJwtAuthenticationApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
