9839d6d2bb387bc9a82b48ae973b59e9ce414235
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-19 15:38:31 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse66/springboot/springbootwithjwtauthentication/SpringBootWithJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.springbootwithjwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithJwtAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse66/springboot/springbootwithjwtauthentication/SpringBootWithJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.springbootwithjwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
